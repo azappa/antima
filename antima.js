4269c5065e9f9d11acfef668393499ce2c39e30f
Alessio Zappa <alessioedoardozappa@gmail.com>
2020-07-28 21:44:00 +0200

    update code examples and libs

--- antima.js
+++ antima.js
@@ -1,100 +1,90 @@
 #!/usr/bin/env node
 
-/*jshint node:true, eqnull:true, laxcomma:true, undef:true, indent:2, camelcase:false */
-'use strict';
-
-
 //  -- node modules --
-var fs = require('fs');
-var del = require('del');
-var pug = require('pug');
-var argv = require('yargs').argv;
-var yaml = require('yamljs');
-
+const fs = require('fs');
+const del = require('del');
+const pug = require('pug');
+const { argv } = require('yargs');
+const yaml = require('yamljs');
 
 //  -- check parameters --
 if (!argv.template || argv.template === '') {
   console.log('Error, you must pass a --template variable to node process.');
   process.exit(1);
-  return;
 }
 
-
 //  -- variables --
-var template = argv.template;
-var yamlPath = './' + template + '/source';
-var htmlPath = './' + template;
-var templatePath = './' + template + '/template';
-
+const { template } = argv;
+const yamlPath = `./${template}/source`;
+const htmlPath = `./${template}`;
+const templatePath = `./${template}/template`;
 
 //  -- check if folder with json files exists --
 if (!fs.existsSync(yamlPath)) {
   console.log('Error, json folder does not exist.');
   process.exit(1);
-  return;
 }
 
-
 //  -- reading data from json files --
-var yamls = fs.readdirSync(yamlPath);
+let yamls = fs.readdirSync(yamlPath);
 if (!yamls || yamls.length === 0) {
   console.log('Error, you have zero json files for data loading.');
   process.exit(1);
-  return;
 }
 
-
 //  -- removing old HTML files --
-del.sync([htmlPath + '/*.html']);
+del.sync([`${htmlPath}/*.html`]);
 
 //  -- array for sitemap links --
-var sitemap = [];
-var sitemapConfig;
+const sitemap = [];
+let sitemapConfig;
 
-if (fs.existsSync(templatePath + '/sitemap.config.json')) {
-  sitemapConfig = fs.readFileSync(templatePath + '/sitemap.config.json', 'utf-8');
+if (fs.existsSync(`${templatePath}/sitemap.config.json`)) {
+  sitemapConfig = fs.readFileSync(
+    `${templatePath}/sitemap.config.json`,
+    'utf-8'
+  );
   sitemapConfig = JSON.parse(sitemapConfig).keys;
 }
 
 //  -- generating sitemap --
-yamls = yamls.filter(function (y) { return y.indexOf('.yaml') !== -1; });
-yamls.forEach(function (file) {
-  var yamlSource = yaml.load(yamlPath + '/' + file);
-  
-  var sitemapItem = {
-    path: htmlPath + '/' + file.replace('.yaml', '.html')
+yamls = yamls.filter((y) => {
+  return y.indexOf('.yaml') !== -1;
+});
+yamls.forEach((file) => {
+  const yamlSource = yaml.load(`${yamlPath}/${file}`);
+  const sitemapItem = {
+    path: `${htmlPath}/${file.replace('.yaml', '.html')}`,
+    relativePath: `./${file.replace('.yaml', '.html')}`,
+    fileName: `${file.replace('.yaml', '.html')}`,
   };
-  
+
   if (sitemapConfig) {
-    sitemapConfig.forEach(function (k) {
+    sitemapConfig.forEach((k) => {
       sitemapItem[k] = yamlSource[k];
     });
   }
-  
+
   sitemap.push(sitemapItem);
 });
 
-
 //  -- writing sitemap into a pug file --
 fs.writeFileSync(
-  templatePath + '/sitemap.pug',
-  '- var sitemap = ' + JSON.stringify(sitemap),
+  `${templatePath}/sitemap.pug`,
+  `- var sitemap = ${JSON.stringify(sitemap)}`,
   'utf-8'
 );
 
-
 // -- build html pages from pug template --
-yamls.forEach(function (file) {
-  var yamlSource = yaml.load(yamlPath + '/' + file);
-  var html = pug.renderFile(
-    templatePath + '/' + template + '.pug', {
-      data: yamlSource,
-      fileName: file.replace('.yaml', '')
-    }
-  );
+yamls.forEach((file) => {
+  const yamlSource = yaml.load(`${yamlPath}/${file}`);
+  const html = pug.renderFile(`${templatePath}/${template}.pug`, {
+    data: yamlSource,
+    fileName: file.replace('.yaml', ''),
+  });
 
   fs.writeFileSync(
-    htmlPath + '/' + file.replace('.yaml', '.html'),
+    `${htmlPath}/${file.replace('.yaml', '.html')}`,
     html,
     'utf-8'
   );
